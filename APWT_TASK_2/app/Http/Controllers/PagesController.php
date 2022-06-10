80753e36cde2bedf6c05cdaa8dc21af16cb41c6b
sadimohammad65@gmail.com <sadimohammad65@gmail.com>
2022-06-10 21:47:49 +0600

    APWT Task2

---
+++ APWT_TASK_2/app/Http/Controllers/PagesController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function home(){
+        return view('home');
+    }
+
+    public function products(){
+        $products=array("Bain Syrup", "Xfin", "Bactrocin","Xymet","Gastrocon-R","Gavisol");
+        return view('products')
+        ->with('products', $products);;
+    }
+
+    public function teams(){
+        $teams=array("Mr. X", "Mr. Y", "Mr. Z", "Mr. A", "Mr. B", "Mr. C");
+        return view('teams')
+        ->with('teams', $teams);;
+    }
+
+    public function aboutUs(){
+        return view('aboutUs');
+    }
+    public function contactUsSubmit(Request $request){
+        $validate=$request->validate([
+            
+            'email'=>'required',
+            'message'=>'required',
+        ],
+        [
+            'email.required'=>'Please put your mail',
+            'message.required'=>'Please put your message',
+        ]
+    );
+        //return view('contactUs');
+    }
+    public function contactUs(){
+        return view('contactUs');
+    }
+}
